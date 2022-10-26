6724476ad7892e4be92bffe2440490128432945c
Jose luis Flores <1000032660@hexaware.com>
2022-10-26 17:00:26 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    //Metodos.funciones,variables y constructores
+    public static void main(String[] args) {
+       //lineas que nos indican una salida a pantalla
+
+        System.out.println(1 + " Este es mi primer codigo!");
+    }
+}
